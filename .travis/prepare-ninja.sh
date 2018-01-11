68f2cfd9de1444cb67d8e4169a4ecb6e3b90afa9
Sergei Zabolotskikh <serggzz@gmail.com>
2018-01-11 21:27:09 +0100

    fix ninja and stuff

---
+++ .travis/prepare-ninja.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+
+if [[ "$TRAVIS_OS_NAME" = "osx" ]]; then
+brew update; brew install ninja;
+else
+wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip -O third_party/ninja-build.zip
+unzip third_party/ninja-build.zip
+fi

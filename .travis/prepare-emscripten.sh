e084522d3f919c137ac5562cc8cb2169a5362c21
Sergei Zabolotskikh <serggzz@gmail.com>
2018-01-11 22:32:46 +0100

    remove debug print

--- .travis/prepare-emscripten.sh
+++ .travis/prepare-emscripten.sh
@@ -4,7 +4,6 @@ set -x
 set -e
 
 wget -qO- https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz | tar xz -C third_party/
-ls third_party
 pushd third_party/emsdk-portable
 
 ./emsdk update

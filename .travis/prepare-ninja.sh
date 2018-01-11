06f74dc724333484ae372485617f82c0827ac6d7
Sergei Zabolotskikh <sergei@adblockplus.org>
2018-01-11 21:45:49 +0100

    still fix ninja

--- .travis/prepare-ninja.sh
+++ .travis/prepare-ninja.sh
@@ -7,5 +7,6 @@ if [[ "$TRAVIS_OS_NAME" = "osx" ]]; then
 brew update; brew install ninja;
 else
 wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip -O third_party/ninja-build.zip
-unzip third_party/ninja-build.zip -d ./
+mkdir third_party/ninja
+unzip third_party/ninja-build.zip -d third_party/ninja
 fi

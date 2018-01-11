43059e1872fc77c6263b100ef4f6ae2d887b1109
Sergei Zabolotskikh <serggzz@gmail.com>
2018-01-11 20:55:05 +0100

    fix prepare emscripten script: typo for pushd

--- .travis/prepare-emscripten.sh
+++ .travis/prepare-emscripten.sh
@@ -4,8 +4,8 @@ set -x
 set -e
 
 wget -qO- https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz | tar xz -C third_party/
-
-pushd third_party/emsdk-portable$
+ls third_party
+pushd third_party/emsdk-portable
 
 ./emsdk update
 ./emsdk install emscripten-tag-1.37.3-64bit

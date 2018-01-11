4c6940685e85ed66e9a34705531877273c062bdf
Sergei Zabolotskikh <serggzz@gmail.com>
2018-01-11 20:47:47 +0100

    add travis for emscripten

---
+++ .travis/prepare-emscripten.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+
+wget -qO- https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz | tar xz -C third_party/
+
+pushd third_party/emsdk-portable$
+
+./emsdk update
+./emsdk install emscripten-tag-1.37.3-64bit
+./emsdk activate emscripten-tag-1.37.3-64bit
+
+pip3 install -q --user meson
+
+popd

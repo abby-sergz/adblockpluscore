e2de345f7deeed3ca1a728a2e879b095ad15dcfe
Sergei Zabolotskikh <serggzz@gmail.com>
2018-01-24 22:57:53 +0100

    fix travis-install.sh

--- .travis/travis-install.sh
+++ .travis/travis-install.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+. ${NVM_DIR}/nvm.sh
+
 set -x
 set -e
 
 nvm install 8.9.4
 mkdir -p third_party
-bash ./prepare-emscripten.sh
-bash ./prepare-ninja.sh
+bash .travis/prepare-emscripten.sh
+bash .travis/prepare-ninja.sh
 pip3 install -q --user meson

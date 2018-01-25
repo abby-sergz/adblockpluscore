21a40ca624588f984bca0cc1ba1b3346cfac7c58
Sergei Zabolotskikh <serggzz@gmail.com>
2018-01-25 11:22:47 +0100

    [WIP] add simply docker config

---
+++ .travis/prepare-docker.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+
+docker --version
+
+mkdir -p third_party/phusion-baseimage-docker
+pushd third_party/phusion-baseimage-docker
+git init
+git fetch https://github.com/phusion/baseimage-docker.git
+git checkout -b master 06e2983ba995c2e5684f8414b861425c7f5e6369 
+sudo docker build --rm -t my-base-ubuntu-image baseimage-docker/image
+popd
+
+sudo docker build --rm -t -f .travis/Dockerfile adblockpluscore-test-image .
+

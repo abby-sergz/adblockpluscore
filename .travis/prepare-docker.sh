4d8ae9cca2976caea5b932c438254ec554ce29bb
Sergei Zabolotskikh <serggzz@gmail.com>
2018-01-25 13:36:31 +0100

    fix docker

--- .travis/prepare-docker.sh
+++ .travis/prepare-docker.sh
@@ -13,5 +13,5 @@ git checkout -b master 06e2983ba995c2e5684f8414b861425c7f5e6369
 docker build --rm -t my-base-ubuntu-image ./image
 popd
 
-docker build --rm -t -f .travis/Dockerfile adblockpluscore-test-image .
+docker build --rm -f -t .travis/Dockerfile adblockpluscore-test-image .
 
